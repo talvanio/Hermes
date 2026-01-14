78fd80d1e24ceae19b27df71dc42036ee1e7f307
talvanio <talvaniufmg@outlook.com>
2026-01-14 19:43:08 -0300

    initial login page

--- hermes-front/app/login/page.tsx
+++ hermes-front/app/login/page.tsx
@@ -3,17 +3,17 @@ import { Input } from '@/components/shadcn/ui/input';
 
 export default function LoginScreen() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="w-96 space-y-6">
-                <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">Hermes: Urban Dispatcher System</h1>
-                <Input className='width=5' placeholder="username" />
-                <div className="flex w-full max-w-sm items-center gap-2">
-                    <Input type="text" placeholder="password" />
-                    <Button type="submit" variant="outline">
-                        login
-                    </Button>
-                </div>
+        <div className="flex flex-col items-center justify-center min-h-screen px-4">
+            <h1 className="scroll-m-20 text-center text-2xl sm:text-3xl md:text-4xl font-extrabold tracking-tight text-balance absolute top-40 sm:top-16 md:top-45 max-w-xs sm:max-w-md md:max-w-2xl">
+                <span className="text-orange-400">HERMES:</span> An Urban Dispatcher System
+            </h1>
 
+            <div className="w-full max-w-sm md:max-w-md space-y-4">
+                <Input placeholder="Username" />
+                <Input type="Password" placeholder="Password" />
+                <Button type="submit" variant="outline" className="w-full h-15">
+                    Login
+                </Button>
             </div>
         </div>
     )
