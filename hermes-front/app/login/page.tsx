b913328b891ab335e2c64bcf0d7a2a40f86288aa
talvanio <talvaniufmg@outlook.com>
2026-01-14 22:08:14 -0300

    feat: Login page temporarily directs to home page

--- hermes-front/app/login/page.tsx
+++ hermes-front/app/login/page.tsx
@@ -1,17 +1,21 @@
+"use client"
 import {Button} from '@/components/shadcn/ui/button';
 import { Input } from '@/components/shadcn/ui/input';
+import { Link } from 'lucide-react';
+import { useRouter } from 'next/navigation'
 
 export default function LoginScreen() {
+    const router = useRouter()
     return (
         <div className="flex flex-col items-center justify-center min-h-screen px-4">
             <h1 className="scroll-m-20 text-center text-2xl sm:text-3xl md:text-4xl font-extrabold tracking-tight text-balance absolute top-40 sm:top-16 md:top-45 max-w-xs sm:max-w-md md:max-w-2xl">
-                <span className="text-orange-400">HERMES:</span> An Urban Dispatcher System
+                <span className="text-orange-400">HERMES:</span> An Urban Dispatching System
             </h1>
 
             <div className="w-full max-w-sm md:max-w-md space-y-4">
                 <Input placeholder="Username" />
                 <Input type="Password" placeholder="Password" />
-                <Button type="submit" variant="outline" className="w-full h-15">
+                <Button onClick={() => router.push("/")} variant="outline" className="w-full h-15">
                     Login
                 </Button>
             </div>
