582a0bae908f04b917a6e6cf7d373e72a9b2fc5d
talvanibarbosa <talvanibarbosa@fundep.com.br>
2026-01-19 19:22:56 -0300

    hermes-front: fix: redirecting all / requests to /login

--- hermes-front/app/page.tsx
+++ hermes-front/app/page.tsx
@@ -1,23 +1,6 @@
-"use client"
-
-import dynamic from "next/dynamic";
-import { useGlobalTheme } from "./ThemeProvider";
-import HermesLoading from "@/components/HermesLoading";
-
-const Map = dynamic(
-  () => import('@/components/Map'),
-  { 
-    loading: () => <HermesLoading />,
-    ssr: false 
-  }
-);
-
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  const { theme } = useGlobalTheme() 
-  return (
-      <div className="h-screen w-full">
-        <Map theme={theme} className="w-screen h-screen" position={[-19.926252,-43.945999]} zoom={16} />
-      </div>
-  );
+  redirect("/login")
+  return null;
 }
