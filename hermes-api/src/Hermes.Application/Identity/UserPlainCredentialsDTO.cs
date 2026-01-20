4605a346d58574bd910e00cbdc8fa299bcfadfa6
talvanibarbosa <talvanibarbosa@fundep.com.br>
2026-01-20 13:56:06 -0300

    refactor(hermes-api): required properties on UserPlainCredentialsDTO and tolower username

--- hermes-api/src/Hermes.Application/Identity/UserPlainCredentialsDTO.cs
+++ hermes-api/src/Hermes.Application/Identity/UserPlainCredentialsDTO.cs
@@ -1,5 +1,8 @@
-public record UserPlainCredentialsDTO
+public record UserPlainCredentialsDTO(string Username, string Password)
 {
-    public required string Username { get; init; }
-    public required string Password { get; init; }
+    public string Username { get; init; } = Username?.ToLower().Trim() 
+        ?? throw new ArgumentException("Username is required", nameof(Username));
+
+    public string Password { get; init; } = Password 
+        ?? throw new ArgumentException("Password is required", nameof(Password));
 }
