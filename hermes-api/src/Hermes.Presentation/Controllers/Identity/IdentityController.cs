d93666589552caa67242dac5ca5759fedfb06279
talvanibarbosa <talvaniufmg@outlook.com>
2026-08-17 20:07:15 -0300

    chore: identation

--- hermes-api/src/Hermes.Presentation/Controllers/Identity/IdentityController.cs
+++ hermes-api/src/Hermes.Presentation/Controllers/Identity/IdentityController.cs
@@ -8,37 +8,35 @@ public static class IdentityController
     public static void MapIdentityRoutes(this IEndpointRouteBuilder app) 
     {
         app.MapPost("/login", async (UserPlainCredentialsDTO plainCredentials, IdentityHandler identityHandler) => 
-        {
-            if (await identityHandler.HandleLoginAsync(plainCredentials))
             {
-                return Results.Ok("Login successful");
-            }
-            return Results.Unauthorized();
-        })
-        .Produces<string>(StatusCodes.Status200OK)
-        .Produces(StatusCodes.Status401Unauthorized)
-        .WithName("Login")
-        .WithTags("Authentication");
+                if (await identityHandler.HandleLoginAsync(plainCredentials))
+                {
+                    return Results.Ok("Login successful");
+                }
+                return Results.Unauthorized();
+            })
+            .Produces<string>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status401Unauthorized)
+            .WithName("Login")
+            .WithTags("Authentication");
 
 
         app.MapPost("/register", async (UserPlainCredentialsDTO plainCredentials, IdentityHandler identityHandler) => 
-        {
-            await identityHandler.HandleRegisterAsync(plainCredentials.Username, plainCredentials.Password);
+            {
+                await identityHandler.HandleRegisterAsync(plainCredentials.Username, plainCredentials.Password);
             
-            return Results.Created($"/users/{plainCredentials.Username}", null);
-        })
-        .Produces(StatusCodes.Status201Created)
-        .WithName("Register")
-        .WithTags("Authentication");
+                return Results.Created($"/users/{plainCredentials.Username}", null);
+            })
+            .Produces(StatusCodes.Status201Created)
+            .WithName("Register")
+            .WithTags("Authentication");
 
         app.MapGet("/logout", () =>
-        {
-            return;
-        })
-        .Produces(StatusCodes.Status200OK)
-        .WithName("Logout")
-        .WithTags("Authentication");
+            {
+                return;
+            })
+            .Produces(StatusCodes.Status200OK)
+            .WithName("Logout")
+            .WithTags("Authentication");
     }
 }
-
-
